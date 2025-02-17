ba74fc0f23eebbc299fd352011f80ba64b57a1d8
adwaith911 <adwaithssundar04@gmail.com>
2025-02-17 13:42:37 +0000

    readme changes

--- model_deploy.py
+++ model_deploy.py
@@ -1,6 +1,6 @@
 from flask import Flask, request, jsonify
 from flask_cors import CORS, cross_origin
-from temp import train_and_predict_sustainability
+from regressionModel import train_and_predict_sustainability
 
 app = Flask(__name__)
 
@@ -13,29 +13,32 @@ app.config['CORS_HEADERS'] = 'Content-Type'
 def process_data(data):
     # Example: reverse the input string if it's a string
     if isinstance(data, str):
-         input_data = {
-    "Company" : 'Adidas',
-    "Product_Type": 3,              # Replace with encoded value
-    "Greenhouse_Gas_Emissions": 4000, 
-    "Pollutants_Emitted": 12, 
-    "Water_Consumption": 5000, 
-    "Energy_Consumption": 1000, 
-    "Waste_Generation": 200
-  }
+
         
-    return train_and_predict_sustainability(data)
+      return train_and_predict_sustainability(data)
 
 @app.route('/api/process', methods=['POST'])
 def api_process():
     try:
         # Get JSON data from the request
-        content = request.get_json(force=True)
-        if not content or 'input' not in content:
-            return jsonify({'error': 'Please provide an "input" field in the JSON payload.'}), 400
+        content = request.get_json()
+        # if not content or 'input' not in content:
+        #     return jsonify({'error': 'Please provide an "input" field in the JSON payload.'}), 400
 
-        input_data = content['input']
-        result = process_data(input_data)
-        return jsonify({'result': result})
+        # input_data = content['input']
+        # result = process_data(input_data)
+        input_data = {
+                "Company" : 'Adidas',
+                "Product_Type": 3,              # Replace with encoded value
+                "Greenhouse_Gas_Emissions": 4000, 
+                "Pollutants_Emitted": 12, 
+                "Water_Consumption": 5000, 
+                "Energy_Consumption": 1000, 
+                "Waste_Generation": 200
+         }
+        result =process_data(input_data)
+        print(result)
+        return jsonify({'result': 0})
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 
